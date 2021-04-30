e140f34d86cdce32876eae6ac3379eaa77175869
robert.kroon <robert.kroon@kentaa.nl>
2021-04-30 09:22:13 +0200

    Add support for initial / recurring payment requests.

--- lib/docdata/order/urls.rb
+++ lib/docdata/order/urls.rb
@@ -5,11 +5,11 @@ module Docdata
     module Urls
       # WSDL location, see Order API 1.3.
       WSDL_LIVE_URL = "https://secure.docdatapayments.com/ps/services/paymentservice/1_3?wsdl"
-      WSDL_TEST_URL = "https://test.docdatapayments.com/ps/services/paymentservice/1_3?wsdl"
+      WSDL_TEST_URL = "https://testsecure.docdatapayments.com/ps/services/paymentservice/1_3?wsdl"
 
       # Payment Menu base URL, see Functional API.
       MENU_LIVE_URL = "https://secure.docdatapayments.com/ps/menu"
-      MENU_TEST_URL = "https://test.docdatapayments.com/ps/menu"
+      MENU_TEST_URL = "https://testsecure.docdatapayments.com/ps/menu"
     end
   end
 end
